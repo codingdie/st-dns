00c3b777c7f92b5d76fb955ac485573772a9fb34
codingdie <codingdie@gmail.com>
2020-06-14 22:29:39 +0800

    update

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -42,9 +42,9 @@ void testParallel(FUNC testMethod, int count) {
     threadPool.join();
     long end = now();
     long timeToal = (end - begin);
-    Logger::INFO << timeToal << timeToal * 1.0 / count << successNum * 1.0 / count * 100 << END;
+    Logger::INFO << "total:" + to_string(timeToal) << "avg:" + to_string(timeToal * 1.0 / count)
+                 << "success:" + to_string(successNum * 1.0 / count * 100) << END;
 }
-
 
 int main(int argc, char *argv[]) {
 
@@ -54,7 +54,7 @@ int main(int argc, char *argv[]) {
 void testUdp() {
     testParallel([]() {
         string domain = "google.com";
-        string server = "127.0.0.1";
+        string server = "192.168.21.1";
         auto dnsResponse = DNSClient::udpDns(domain, server);
         if (dnsResponse != nullptr) {
             Logger::INFO << "success" << dnsResponse->header->id
@@ -74,7 +74,8 @@ void testTcp() {
         auto tcpDnsResponse = DNSClient::tcpDns(domain, server);
         if (tcpDnsResponse != nullptr) {
             UdpDNSResponse *dnsResponse = tcpDnsResponse->udpDnsResponse;
-            Logger::INFO << "success" << dnsResponse->header->id << dnsResponse->queryZone->querys.front()->domain->domain << dnsResponse->header->id
+            Logger::INFO << "success" << dnsResponse->header->id
+                         << dnsResponse->queryZone->querys.front()->domain->domain << dnsResponse->header->id
                          << ipsToStr(dnsResponse->ips) << END;
             delete tcpDnsResponse;
             return true;

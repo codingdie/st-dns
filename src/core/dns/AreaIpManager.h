91fde7f7fce51bc21a5dfbb7604c2d3584aeecc6
codingdie <codingdie@gmail.com>
2020-10-11 14:17:52 +0800

    chore: change impl to fix asio ssl handshake cancel bug

--- src/core/dns/AreaIpManager.h
+++ src/core/dns/AreaIpManager.h
@@ -2,8 +2,8 @@
 // Created by System Administrator on 2020/10/8.
 //
 
-#ifndef ST_DNS_COUNTRYIPMANAGER_H
-#define ST_DNS_COUNTRYIPMANAGER_H
+#ifndef ST_DNS_AREAIPMANAGER_H
+#define ST_DNS_AREAIPMANAGER_H
 
 #include <set>
 #include <unordered_map>
@@ -15,14 +15,14 @@
 using namespace std;
 
 
-class CountryIpManager {
+class AreaIpManager {
 private:
-    static CountryIpManager INSTANCE;
+    static AreaIpManager INSTANCE;
     unordered_map<string, vector<pair<uint32_t, uint32_t>> *> caches;
 public:
-    static bool isCountryIP(const string &countryCode, const uint32_t &ip);
+    static bool isAreaIP(const string &areaCode, const uint32_t &ip);
 
 };
 
 
-#endif //ST_DNS_COUNTRYIPMANAGER_H
+#endif //ST_DNS_AREAIPMANAGER_H
--- src/core/utils/ByteUtils.h
+++ src/core/utils/ByteUtils.h
@@ -6,13 +6,80 @@
 #define ST_BYTEUTILS_H
 
 #include <iostream>
+#include <string>
+#include <vector>
 
+using namespace std;
 typedef uint8_t byte;
 
-static void copyByte(byte *from, byte *to, int size) {
-    for (int i = 0; i < size; i++) {
-        *(to + i) = *(from + i);
+namespace st {
+    namespace utils {
+        static void copyByte(byte *from, byte *to, int size) {
+            for (int i = 0; i < size; i++) {
+                *(to + i) = *(from + i);
+            }
+        }
+
+        template<typename ItemType, typename Num> static void copy(ItemType *from, ItemType *to, Num len) {
+            for (auto i = 0; i < len; i++) {
+                *(to + i) = *(from + i);
+            }
+        };
+
+        template<typename ItemType, typename Num> static void copy(const char *from, ItemType &to, Num len) {
+            for (auto i = 0; i < len; i++) {
+                to[i] = *(from + i);
+            }
+        };
+
+        template<typename ItemType, typename ItemTypeB, typename NumA, typename NumB, typename NumC> static void copy(ItemType *from, ItemTypeB *to,
+                                                                                                                      NumA indexFrom, NumB distFrom,
+                                                                                                                      NumC len) {
+            for (auto i = 0; i < len; i++) {
+                *(to + distFrom + i) = *(from + indexFrom + i);
+            }
+        };
+
+        template<typename Num> static void toBytes(byte *byteArr, Num num) {
+            uint8_t len = sizeof(Num);
+            for (auto i = 0; i < len; i++) {
+                uint64_t move = (len - i - 1) * 8U;
+                uint64_t mask = 0xFFU << move;
+                *(byteArr + i) = (num & mask) >> move;
+            }
+        };
+
+        template<typename IntTypeB> static void read(const byte *data, IntTypeB &result) {
+            uint8_t len = sizeof(IntTypeB);
+            for (uint8_t i = 0; i < len; i++) {
+                byte val = *(data + i);
+                uint32_t bitMove = (len - i - 1) * 8U;
+                uint32_t valFinal = val << bitMove;
+                result |= valFinal;
+            }
+        }
+
+        static string join(vector<string> &lists, const char *delimit) {
+            string result;
+            for (auto value : lists) {
+                if (!result.empty()) {
+                    result += delimit;
+                }
+                result += value;
+            }
+            return result;
+        }
+
+        template<typename IntTypeB> static byte *write(byte *data, IntTypeB &result) {
+            uint8_t len = sizeof(IntTypeB);
+            for (uint8_t i = 0; i < len; i++) {
+                auto i1 = (len - i - 1) * 8;
+                *(data + i) = (((0xFF << i1) & result) >> i1);
+            }
+            return data + len;
+        }
     }
 }
+
 
 #endif //ST_BYTEUTILS_H

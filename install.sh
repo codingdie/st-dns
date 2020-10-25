2fcfe7a7f00ed723b8b1a0a2ac580e03c8ed3ebd
codingdie <codingdie@gmail.com>
2020-10-25 11:32:18 +0800

    chore: support ubuntu

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 set -e
 if [ ! -d "build" ]; then
-    mkdir build
+  mkdir build
 fi
 CMAKE_INSTALL_PREFIX="/usr/local"
-if [ "" != "$1" ];then
+if [ "" != "$1" ]; then
   CMAKE_INSTALL_PREFIX=$1
 fi
 cd build
-cmake  -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}  ../
+cmake -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} ../
 make -j8
-sudo make install
+make install

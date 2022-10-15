9499a5bce8629d90c3930f2f7c0305ff81fb04b7
codingdie <codingdie@gmail.com>
2022-10-15 17:45:24 +0800

    fix kv header

--- src/common/main/kv/abstract_kv.h
+++ src/common/main/kv/abstract_kv.h
@@ -1,15 +1,16 @@
 #ifndef ST_ABSTRACT_KV_H
 #define ST_ABSTRACT_KV_H
 
+#include <functional>
 #include <string>
-
+#include <utility>
 namespace st {
     namespace kv {
         class abstract_kv {
         public:
-            abstract_kv(const std::string &ns, uint32_t max_size) : ns(ns), max_size(max_size) {}
+            abstract_kv(std::string ns, uint32_t max_size) : ns(std::move(ns)), max_size(max_size) {}
 
-            virtual ~abstract_kv() {}
+            virtual ~abstract_kv() = default;
 
             virtual std::string get(const std::string &key) { return ""; }
 

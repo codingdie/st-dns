6f3377a9b04c88cfa498f17eb8b2f3c789afb51d
codingdie <codingdie@gmail.com>
2023-10-08 19:26:12 +0800

    refactor

--- src/core/protocol/dns_message.h
+++ src/core/protocol/dns_message.h
@@ -137,7 +137,7 @@ namespace st {
                         head->len = DEFAULT_LEN;
                         head->qr = (*(data + 2) & (0b10000000));
                         head->opcode = ((*(data + 2) & (0b01111000)) >> 3);
-                        head->responseCode = (*(data + 3) & 0x01F);
+                        head->responseCode = (*(data + 3) & 0b1111);
                         st::utils::read(data + 4, head->questionCount);
                         st::utils::read(data + 6, head->answerCount);
                         return head;

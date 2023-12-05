85e2e998af4622f419701fb6ff7287e5987c82ac
codingdie <codingdie@gmail.com>
2023-12-05 21:36:47 +0800

    refactor task queue

--- src/core/dns_server.h
+++ src/core/dns_server.h
@@ -14,7 +14,7 @@
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
-
+#include "taskquque/task_queue.h"
 using namespace boost::asio;
 using namespace boost::asio::ip;
 using namespace std;
@@ -36,10 +36,10 @@ private:
     udp::socket *ss = nullptr;
     io_context ic;
     boost::asio::io_context::work *iw;
-    unordered_map<string, unordered_set<session *>> waiting_sessions;
     std::atomic<uint8_t> state;
     atomic_int64_t counter;
     st::console::udp_console console;
+    st::task::queue<string> sync_remote_record_task_queue;
 
     void receive();
 
@@ -47,9 +47,8 @@ private:
 
     void query_dns_record(session *session, const std::function<void(st::dns::session *)> &complete_handler);
 
-    void sync_dns_record_from_remote(const string &host, const std::function<void(dns_record record)> &complete, vector<remote_dns_server *> servers, int pos, bool return_resolve_any);
+    void sync_dns_record_from_remote(const string &host, const std::function<void(dns_record record)> &complete, vector<remote_dns_server *> servers, int pos);
 
-    void query_dns_record_from_remote(session *session, const std::function<void(st::dns::session *)> &complete_handler);
 
     void forward_dns_request(session *session, const std::function<void(st::dns::session *)> &complete_handler);
 
@@ -57,15 +56,12 @@ private:
 
     void end_session(session *session);
 
-    void update_dns_record(const string &domain);
 
-    bool begin_query_remote(const string &domain, session *session);
-
-    unordered_set<session *> end_query_remote(const string &host);
 
     uint32_t cal_expire(dns_record &record) const;
 
     void start_console();
+    dns_record query_record_from_cache(const string &host) const;
 };
 
 
